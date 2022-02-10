08ac4de99f415265cef4f902854a405d33142dee
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-02-10 13:46:42 +0100

    Added `UUID.generate()`.

---
+++ Sources/FoundationToolkit/officially_vended_products/_package_extensions/Foundation/UUID+extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UUID+extensions.swift
+//  
+//
+//  Created by Jeremy Bannister on 1/24/22.
+//
+
+///
+public extension UUID {
+    
+    ///
+    static func generate () -> Self {
+        .init()
+    }
+}
