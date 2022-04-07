ee930b01e5cd505149912754ef22b3d3b42776bb
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-04-07 12:12:27 +0200

    Added `Task.sleep(seconds:)`.

---
+++ Sources/FoundationToolkit/officially_vended_products/_package_extensions/Foundation/Task+extensions.swift
@@ -0,0 +1,27 @@
+//
+//  Task+extensions.swift
+//  
+//
+//  Created by Jeremy Bannister on 4/7/22.
+//
+
+///
+@available(iOS 13.0, macOS 10.15.0, *)
+public extension Task
+    where Success == Never,
+          Failure == Never {
+    
+    ///
+    static func sleep (seconds: Double) async throws {
+        try await Task.sleep(nanoseconds: UInt64(seconds.times(.numberOfNanosecondsInSecond)))
+    }
+}
+
+///
+fileprivate extension Double {
+    
+    ///
+    static var numberOfNanosecondsInSecond: Self {
+        1_000_000_000
+    }
+}
