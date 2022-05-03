75c704d5ecd74add630ff7ef383b1d9d7750815d
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-05-03 10:29:24 +0200

    `Result` now conforms to `Codable` where its `Success` and `Failure` types conform to `Codable`.

--- Sources/FoundationToolkit/officially_vended_products/_package_extensions/Foundation/Result+extensions.swift
+++ Sources/FoundationToolkit/officially_vended_products/_package_extensions/Foundation/Result+extensions.swift
@@ -21,3 +21,40 @@ public extension Result {
         }
     }
 }
+
+///
+extension Result: Codable where Success: Codable, Failure: Codable {
+    
+    ///
+    public func encode (to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .success (let successfulValue):
+            try container.encode(successfulValue, forKey: .success)
+            
+        case .failure (let error):
+            try container.encode(error, forKey: .failure)
+        }
+    }
+    
+    ///
+    public init (from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        do {
+            let success = try container.decode(Success.self, forKey: .success)
+            self = .success(success)
+        } catch {
+            if let failure = try? container.decode(Failure.self, forKey: .failure) {
+                self = .failure(failure)
+            } else {
+                throw error
+            }
+        }
+    }
+    
+    ///
+    private enum CodingKeys: CodingKey {
+        case success
+        case failure
+    }
+}
