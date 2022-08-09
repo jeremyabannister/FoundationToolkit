7708e03d30186a05e992cfe7e868a6240b321ac2
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-08-09 13:55:55 +0200

    Added `Dictionary.pasteIn(_:)` and `Dictionary.pastingIn(_:)`.

--- Sources/FoundationToolkit/officially_vended_products/_package_extensions/Foundation/Dictionary_Keys+extensions.swift
+++ Sources/FoundationToolkit/officially_vended_products/_package_extensions/Foundation/Dictionary_Keys+extensions.swift
@@ -6,6 +6,22 @@
 //
 
 ///
+public extension Dictionary {
+    
+    /// Write all keys and values from otherDict into the receiver, overwriting previous key-value pairs which collide.
+    mutating func pasteIn (_ otherDict: Self) {
+        otherDict.forEach { self[$0] = $1 }
+    }
+    
+    /// Write all keys and values from otherDict into a copy of the receiver, overwriting previous key-value pairs which collide, and return the result
+    func pastingIn (_ otherDict: Self) -> Self {
+        var copy = self
+        copy.pasteIn(otherDict)
+        return copy
+    }
+}
+
+///
 extension Dictionary.Keys: Hashable {
     
     ///
