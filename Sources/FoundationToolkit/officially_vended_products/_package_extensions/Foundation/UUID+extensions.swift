c6fc48202ce08e768c5479d18ee871c90eec8479
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-09-06 23:23:23 +0200

    Deprecated `UUID.generate()` in favor of more specifically named `UUID.generateRandom()`.

--- Sources/FoundationToolkit/officially_vended_products/_package_extensions/Foundation/UUID+extensions.swift
+++ Sources/FoundationToolkit/officially_vended_products/_package_extensions/Foundation/UUID+extensions.swift
@@ -1,6 +1,6 @@
 //
 //  UUID+extensions.swift
-//  
+//
 //
 //  Created by Jeremy Bannister on 1/24/22.
 //
@@ -9,7 +9,13 @@
 public extension UUID {
     
     ///
-    static func generate () -> Self {
+    static func generateRandom () -> Self {
         .init()
+    }
+    
+    ///
+    @available(*, deprecated, message: "This method has been renamed to .generateRandom(), because that is actually what it does, and the word generate by itself is too broad.")
+    static func generate () -> Self {
+        .generateRandom()
     }
 }
