eea78ffb0a4dc0bf63bc44288ec92dfad5d3bd08
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-09-18 03:16:54 +0200

    Added `MutableCollection.mutatingLast(_:) where Self: BidirectionalCollection`.

--- Sources/FoundationToolkit/officially_vended_products/_package_extensions/Foundation/BidirectionalCollection+extensions.swift
+++ Sources/FoundationToolkit/officially_vended_products/_package_extensions/Foundation/BidirectionalCollection+extensions.swift
@@ -5,19 +5,6 @@
 //  Created by Jeremy Bannister on 12/9/21.
 //
 
-// MARK: - mutateLast
-
-///
-public extension BidirectionalCollection where Self: MutableCollection {
-    
-    ///
-    mutating func mutateLast (_ mutation: (inout Element)->()) {
-        guard var last = self.last else { return }
-        mutation(&last)
-        self[self.index(before: self.endIndex)] = last
-    }
-}
-
 // MARK: - Iteration "WithIndex"
 
 ///
