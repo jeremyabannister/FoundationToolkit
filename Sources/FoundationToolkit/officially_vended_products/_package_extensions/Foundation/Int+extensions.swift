1c6ad5731f83b5b987e68557f8c95e0cb83d3f37
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-09-23 14:36:40 +0200

    Added `Int.seconds` for writing expressions like `7.seconds`.

---
+++ Sources/FoundationToolkit/officially_vended_products/_package_extensions/Foundation/Int+extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Int+extensions.swift
+//  
+//
+//  Created by Jeremy Bannister on 9/23/22.
+//
+
+///
+public extension Int {
+    
+    ///
+    var seconds: TimeInterval {
+        Double(self)
+    }
+}
