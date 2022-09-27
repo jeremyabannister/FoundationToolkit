6e590948954b9fcd7583867079fba1527028b274
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-09-27 15:12:10 +0200

    Updated to `EmptyCodableModule` version `0.1.1`.

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         ///
         .package(
             url: "https://github.com/jeremyabannister/EmptyCodableModule",
-            from: "0.1.0"
+            from: "0.1.1"
         ),
         
         ///
