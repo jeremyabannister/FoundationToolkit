325c74f28ea081013b8db744e2d3727e72441d9d
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-05-29 20:54:38 +0200

    Added `AdditiveArithmetic.negated`.

---
+++ Sources/FoundationToolkit/officially_vended_products/_package_extensions/Foundation/AdditiveArithmetic+extensions.swift
@@ -0,0 +1,15 @@
+//
+//  AdditiveArithmetic+extensions.swift
+//  
+//
+//  Created by Jeremy Bannister on 5/29/23.
+//
+
+///
+extension AdditiveArithmetic {
+    
+    ///
+    public var negated: Self {
+        .zero - self
+    }
+}
