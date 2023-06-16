3bce78799e3da89849cd44410d7da35417e211d1
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-06-16 18:03:01 +0100

    Added `StringProtocol.asString()`.

---
+++ Sources/FoundationToolkit/officially_vended_products/_package_extensions/Foundation/StringProtocol+extensions.swift
@@ -0,0 +1,15 @@
+//
+//  StringProtocol+extensions.swift
+//  
+//
+//  Created by Jeremy Bannister on 6/16/23.
+//
+
+///
+extension StringProtocol {
+    
+    ///
+    public func asString () -> String {
+        String(self)
+    }
+}
